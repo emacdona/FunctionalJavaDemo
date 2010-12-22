aa5f3ee35a8156f90077502fb7591283b553a0cf
Ed MacDonald <edmacdonald555@gmail.com>
2010-12-22 01:50:07 -0500

    more fun

--- src/main/java/net/edmacdonald/functionalJavaDemo/FunctionalJavaDemo.java
+++ src/main/java/net/edmacdonald/functionalJavaDemo/FunctionalJavaDemo.java
@@ -40,18 +40,34 @@ public class FunctionalJavaDemo {
         System.out.println("Numbers divisible by 2 and 5");
         integers.filter(S.divisibleByFilter(2)).filter(S.divisibleByFilter(5)).map(S.<Integer>display());
 
+        System.out.println();
+        System.out.println("Sum");
+        System.out.println(
+                integers.foldLeft1(S.intAdder())
+        );
     }
 }
 
 class S{ /* S for "static" cuz this class has a bunch of static methods */
+
+    /*
+     *  Transforms (Things that can be mapped)
+     */
+
     public static <T> F<T,T> display() {
         return new F<T, T>(){
             public T f(T t) {
                 System.out.println(t);
-                return t;
+                return t; //The signature says we have to return a value. This is the best value to return; it will
+                          //allow us to chain operations on a list. This function is a great depiction of a side
+                          //effect.
             }
         };
     }
+
+    /*
+     *  Filters
+     */
 
     public static F<Integer, Boolean> oddFilter() {
         return new F<Integer, Boolean>(){
@@ -77,6 +93,24 @@ class S{ /* S for "static" cuz this class has a bunch of static methods */
         };
     }
 
+    /*
+     *  Folders
+     */
+
+    //Damn, that signature is intimidating! But, once you're forced to implement it, it makes a little more sense.
+    public static F<Integer, F<Integer, Integer>> intAdder() {
+        return new F<Integer, F<Integer, Integer>>(){
+            public F<Integer, Integer> f(final Integer int_a) {
+                return new F<Integer, Integer>(){
+                    public Integer f(Integer int_b) {
+                        return int_a + int_b;
+                    }
+                };
+            }
+        };
+    }
+
+    //Utils
     public static <T> List<T> convertList(java.util.List<T> list){
         return Java.<T>ArrayList_List().f(new ArrayList<T>(list));
     }
